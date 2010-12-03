7697c2080a2a0d00fe8bfb46c8d000e750d1a452
Hongli Lai (Phusion) <hongli@phusion.nl>
2010-12-03 20:05:54 +0100

    Add some comments.

--- Horspool.cpp
+++ Horspool.cpp
@@ -1,3 +1,29 @@
+/*
+ * Originally written by Joel Yliluoma <joel.yliluoma@iki.fi>
+ * http://en.wikipedia.org/wiki/Talk:Boyer%E2%80%93Moore_string_search_algorithm#Common_functions
+ *
+ * Copyright (c) 2008 Joel Yliluoma
+ * Copyright (c) 2010 Hongli Lai
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #include <vector>
  
 typedef std::vector<size_t> occtable_type;
@@ -47,7 +73,8 @@ const size_t SearchInHorspool(const unsigned char* haystack, const size_t haysta
  
         // The author modified this part. Original algorithm matches needle right-to-left.
         // This code calls memcmp() (usually matches left-to-right) after matching the last
-        // character, thereby incorporating some ideas from "Tuning the Boyer–Moore–Horspool String Searching Algorithm"
+        // character, thereby incorporating some ideas from
+        // "Tuning the Boyer-Moore-Horspool String Searching Algorithm"
         // by Timo Raita, 1992.
         if(last_needle_char == occ_char
         && std::memcmp(needle, haystack+haystack_position, needle_length_minus_1) == 0)
